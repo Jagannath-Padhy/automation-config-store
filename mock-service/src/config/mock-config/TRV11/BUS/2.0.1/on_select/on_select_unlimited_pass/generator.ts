17736785cee33a68c2f2637d97f9891405507e5b
“sourabhsaini11” <ss1841620@gmail.com>
2025-11-21 19:14:20 +0530

    fix:TRV11 metro & bus changes

--- mock-service/src/config/mock-config/TRV11/BUS/2.0.1/on_select/on_select_unlimited_pass/generator.ts
+++ mock-service/src/config/mock-config/TRV11/BUS/2.0.1/on_select/on_select_unlimited_pass/generator.ts
@@ -1,39 +1,97 @@
 import { SessionData } from "../../../../session-types";
 
+const createQuoteFromItems = (items: any): any => {
+  let totalPrice = 0;
+  const currency = items[0]?.price?.currency || "INR";
 
+  const breakup = items.map((item: any) => {
+    const itemTotalPrice =
+      Number(item.price?.value || 35) * item.quantity.selected.count;
+    totalPrice += itemTotalPrice;
 
+    return {
+      title: "BASE_FARE",
+      item: {
+        id: item.id,
+        price: {
+          currency,
+          value: item.price?.value || 35,
+        },
+        quantity: {
+          selected: {
+            count: item.quantity.selected.count,
+          },
+        },
+      },
+      price: {
+        currency,
+        value: itemTotalPrice.toFixed(2),
+      },
+    };
+  });
 
+  // Add OFFER and TOLL to breakup
+  breakup.push(
+    {
+      title: "OFFER",
+      price: {
+        currency,
+        value: "0",
+      },
+    },
+    {
+      title: "TOLL",
+      price: {
+        currency,
+        value: "0",
+      },
+    }
+  );
+
+  return {
+    price: {
+      value: totalPrice.toFixed(2),
+      currency,
+    },
+    breakup,
+  };
+};
 
 export async function onSelectGenerator(
-	existingPayload: any,
-	sessionData: SessionData
+  existingPayload: any,
+  sessionData: SessionData
 ) {
+  existingPayload.message.order.items.forEach((item: any, index: number) => {
+    item.time.timestamp = new Date(Date.now()).toISOString();
+    item.time.range.start = new Date(Date.now()).toISOString();
+    item.time.range.end = new Date(Date.now() + 3 * 60 * 60 * 60).toISOString();
+    item.quantity.selected.count =
+      sessionData?.items[index]?.quantity?.selected?.count || 1;
+  });
+  existingPayload.message.order.fulfillments = sessionData.fulfillments.map(
+    (fulfillment) => ({
+      ...fulfillment,
+      stops: [
+        {
+          type: "START",
+          location: {
+            descriptor: {
+              code: "std:011",
+            },
+            gps: "28.666576, 77.233332",
+          },
+          id: "1",
+        },
+      ],
+      vehicle: {
+        category: "BUS",
+        variant: "AC",
+      },
+    })
+  );
 
-	existingPayload.message.order.items.forEach((item:any) => {
-		item.time.timestamp= new Date(Date.now()).toISOString()
-		item.time.range.start=new Date(Date.now()).toISOString()
-		item.time.range.end=new Date(Date.now()+ 3*60*60*60).toISOString()
-	});
-	existingPayload.message.order.fulfillments = sessionData.fulfillments.map(fulfillment => ({
-		...fulfillment,
-		stops: [
-		  {
-			type: "START",
-			location: {
-			  descriptor: {
-				code: "std:011"
-			  },
-			  gps: "28.666576, 77.233332"
-			},
-			id: "1"
-		  }
-		],
-		vehicle: {
-		  category: "BUS",
-		  variant: "AC"
-		}
-	  }));
-	  
-	
-	return existingPayload;
+  const quote = createQuoteFromItems(existingPayload.message.order.items);
+  existingPayload.message.order.quote = quote;
+
+  return existingPayload;
 }
