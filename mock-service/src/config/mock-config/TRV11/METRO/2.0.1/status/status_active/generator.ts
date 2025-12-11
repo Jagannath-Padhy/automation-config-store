1cfba4acc650babe1db90b38f51ad766a13b0b53
Sharique <sharique@WILASSETA0046.local>
2025-12-11 14:29:30 +0530

    fix: issue ref_id issue

--- mock-service/src/config/mock-config/TRV11/METRO/2.0.1/status/status_active/generator.ts
+++ mock-service/src/config/mock-config/TRV11/METRO/2.0.1/status/status_active/generator.ts
@@ -1,9 +1,14 @@
-
-
-export async function statusActiveGenerator(existingPayload: any,sessionData: any){
-    // if(sessionData.order_id){
-    //     existingPayload.message.order_id = sessionData.order_id
-    // }
-    existingPayload.message.ref_id = existingPayload?.context?.transaction_id ?? "123456789"
-    return existingPayload;
+export async function statusActiveGenerator(
+  existingPayload: any,
+  sessionData: any
+) {
+  // if(sessionData.order_id){
+  //     existingPayload.message.order_id = sessionData.order_id
+  // }
+  existingPayload.message = {
+    ref_id:
+      existingPayload?.context?.transaction_id ??
+      "6f339232-2bc3-44d2-915c-30d2b053ce1d",
+  };
+  return existingPayload;
 }
