bb6532508e01e17e1556be37f2365488a51e00d9
Sharique <sharique@WILASSETA0046.local>
2025-12-11 16:32:58 +0530

    fix: added cancellation status call

--- mock-service/src/config/mock-config/TRV11/METRO/2.0.1/status/status_tech_cancel/generator.ts
+++ mock-service/src/config/mock-config/TRV11/METRO/2.0.1/status/status_tech_cancel/generator.ts
@@ -1,9 +1,7 @@
 
 
 export async function statusTechCancelGenerator(existingPayload: any,sessionData: any){
-    console.log('++++++++++');
     if(sessionData.transaction_id){
-        console.log('sessionData.transaction_id---->>>>>>>>>',sessionData.transaction_id);
         existingPayload.message.ref_id = sessionData?.transaction_id ?? "123456789"
     }
     return existingPayload;
