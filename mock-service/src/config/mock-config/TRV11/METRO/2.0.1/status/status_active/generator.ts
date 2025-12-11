5737c2be5f0ff57fc22af4d6cbc4b66f869ef204
Sharique <sharique@WILASSETA0046.local>
2025-12-11 14:21:29 +0530

    fix: issue ref_id issue

--- mock-service/src/config/mock-config/TRV11/METRO/2.0.1/status/status_active/generator.ts
+++ mock-service/src/config/mock-config/TRV11/METRO/2.0.1/status/status_active/generator.ts
@@ -1,8 +1,9 @@
 
 
 export async function statusActiveGenerator(existingPayload: any,sessionData: any){
-    if(sessionData.order_id){
-        existingPayload.message.order_id = sessionData.order_id
-    }
+    // if(sessionData.order_id){
+    //     existingPayload.message.order_id = sessionData.order_id
+    // }
+    existingPayload.message.ref_id = existingPayload?.context?.transaction_id ?? "123456789"
     return existingPayload;
 }
