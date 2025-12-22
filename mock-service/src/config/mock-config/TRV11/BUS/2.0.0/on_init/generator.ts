1166bd2b927daaa502a0a6c7f51a4a0b0b3d9f90
Shivang Dudani <shivangdudani113@gmail.com>
2025-12-22 16:14:13 +0530

    MANDATORY ARBITRATION TO BE TRUE

--- mock-service/src/config/mock-config/TRV11/BUS/2.0.0/on_init/generator.ts
+++ mock-service/src/config/mock-config/TRV11/BUS/2.0.0/on_init/generator.ts
@@ -65,7 +65,7 @@ export async function onInitGenerator(
               descriptor: {
                 code: "MANDATORY_ARBITRATION",
               },
-              value: "true",
+              value: "TRUE",
             },
             {
               descriptor: {
